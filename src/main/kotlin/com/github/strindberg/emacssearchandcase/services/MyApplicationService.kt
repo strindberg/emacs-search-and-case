6ba79f1cae53d081d5d1ba18fbe9992ab48b4b83
GitHub Action <action@github.com>
2022-05-28 06:18:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/strindberg/emacssearchandcase/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.strindberg.emacssearchandcase.services
+
+import com.github.strindberg.emacssearchandcase.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
